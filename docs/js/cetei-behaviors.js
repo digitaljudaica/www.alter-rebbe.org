278956722ad57c8d5a74d310bb84ff25af32ddf7
Leonid Dubinsky <dub@podval.org>
2019-04-04 13:28:23 -0400

    - only one layout uses non-main CSS: tei; - CSS comes in 2 variants - normal and wide (page.wide == true); - header.html adds navigation regardless of the layout used if any of the prev, self and next are present; - pruned layouts, including the stock ones; - facsimile viewers are in a separate subdirectory of a collection ('facs'); - remove XML namespace from nodes transplanted from documents into index. - cleaned up table columns; - added classes to the table columns and use them for sizing; - added `role="documentViewer"` to document/page `ref`s in the collection index; - added behavior that copies `ref`'s `role` into the `a`'s `target`; - added `target='namesViewer'` to `a`s generated from name references; `documentViewer` and `nameViewer` tabs are now re-used (#26).

--- docs/js/cetei-behaviors.js
+++ docs/js/cetei-behaviors.js
@@ -10,7 +10,11 @@ export default {
     // @href is piped through the rw (rewrite) function before insertion
     "ptr": ["<a href=\"$rw@target\">$@target</a>"],
     // wraps the content of the <ref> in an HTML link
-    "ref": ["<a href=\"$rw@target\">","</a>"],
+    // LMD ref with 'role' - which becomes 'target' on the 'a'
+    "ref": [
+      ["[role]", ["<a href=\"$rw@target\" target=\"$@role\">","</a>"]],
+      ["_", ["<a href=\"$rw@target\">","</a>"]]
+    ],
     "graphic": function(elt) {
       let content = new Image();
       content.src = this.rw(elt.getAttribute("url"));
