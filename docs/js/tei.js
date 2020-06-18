74418ace1b7667ad080034a012d114a08144ec10
Leonid Dubinsky <dub@podval.org>
2020-06-18 00:50:25 -0400

    - fix (locally; see https://github.com/TEIC/CETEIcean/pull/37) wrong `this` in behavior functions; - switch to the current CETEIcean; - remove old `CETEI.js`;

--- docs/js/tei.js
+++ docs/js/tei.js
@@ -1,30 +1,29 @@
-//import CETEI from './CETEI/CETEI.js'
-import CETEI from './CETEI.js'
+import CETEI from './CETEI/CETEI.js'
 
 export default function loadTei(tei) {
-    let wrapper = document
-        .getElementsByClassName("page-content").item(0)
-        .getElementsByClassName("wrapper").item(0);
+  let wrapper = document
+    .getElementsByClassName("page-content").item(0)
+    .getElementsByClassName("wrapper").item(0);
 
-    let teiDiv = document.createElement("div");
-    teiDiv.innerHTML = "This page will only work in modern browsers.";
-    wrapper.appendChild(teiDiv);
+  let teiDiv = document.createElement("div");
+  teiDiv.innerHTML = "This page will only work in modern browsers.";
+  wrapper.appendChild(teiDiv);
 
-    let CETEIcean = new CETEI();
+  let cetei = new CETEI();
 
-    let refBehavior    = [["[role][target]", ["<a target='$@role' href='$@target'>", "</a>"]]];
-    CETEIcean.addBehaviors({
-        "tei": {
-            "pb"       : [["[role][target]", ["<a target='$@role' href='$@target'>⎙</a>"]]],
-            "persName" : refBehavior,
-            "placeName": refBehavior,
-            "orgName"  : refBehavior,
-            "supplied" : ["[", "]"]
-        }
-    });
+  let refBehavior = [["[role][target]", ["<a target='$@role' href='$@target'>", "</a>"]]];
+  cetei.addBehaviors({
+    "tei": {
+      "pb"        : [["[role][target]", ["<a target='$@role' href='$@target'>⎙</a>"]]],
+      "persName"  : refBehavior,
+      "placeName" : refBehavior,
+      "orgName"   : refBehavior,
+      "supplied"  : ["[", "]"]
+    }
+  });
 
-    CETEIcean.getHTML5(tei, function (data) {
-        teiDiv.innerHTML = "";
-        teiDiv.appendChild(data);
-    });
+  cetei.getHTML5(tei, function (data) {
+    teiDiv.innerHTML = "";
+    teiDiv.appendChild(data);
+  });
 }
