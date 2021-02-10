c32aee00ac81dffaa507d49b42b32f951c0ce4ab
Leonid Dubinsky <dub@podval.org>
2021-02-10 00:48:41 -0500

    - static files are no longer generated; (#2850)

--- src/main/scala/org/opentorah/collector/Main.scala
+++ src/main/scala/org/opentorah/collector/Main.scala
@@ -9,52 +9,38 @@ object Main {
   private val logger: Logger = LoggerFactory.getLogger(this.getClass)
   private def info(message: String): Unit = logger.info(message)
 
-  def main(args: Array[String]): Unit =
-    if (args.isEmpty) Service.main(Array.empty) else {
-      val siteRootPath: String = args(1)
+  def main(args: Array[String]): Unit = if (args.isEmpty) Service.main(Array.empty) else {
+    val siteRootPath: String = args(1)
 
-      args(0) match {
-        case "serve"    => Service.main(Array(siteRootPath))
-        case "verify"   => generateSite(siteRootPath, doPrettyPrint = true , doWrite = false)
-        case "generate" => generateSite(siteRootPath, doPrettyPrint = false, doWrite = true )
-        case "build"    => generateSite(siteRootPath, doPrettyPrint = true , doWrite = true )
-        case "upload"   =>
-          GoogleCloudStorageSynchronizer.sync(
-            serviceAccountKey = args(2),
-            Service.bucketName,
-            bucketPrefix = "",
-            directoryPath = siteRootPath + "/",
-            dryRun = (args.length > 3) && (args(3) == "dryRun")
-          )
+    args(0) match {
+      case "serve" =>
+        Service.main(Array(siteRootPath))
 
-        case command => throw new IllegalArgumentException(s"Unrecognized command [$command]")
-      }
-    }
+      case "build" =>
+        val baseUrl: URL = new File(siteRootPath).toURI.toURL
 
-  private def generateSite(
-    siteRootPath: String,
-    doPrettyPrint: Boolean,
-    doWrite: Boolean
-  ): Unit = {
-    val baseUrl: URL = new File(siteRootPath).toURI.toURL
+        info("Reading site.")
+        val site: Site = Site.read(baseUrl)
 
-    info("Reading site.")
-    val site: Site = Site.read(baseUrl)
+        info("Writing site lists.")
+        site.writeLists()
 
-    info("Writing site lists.")
-    site.writeLists()
+        info("Pretty-printing site.")
+        site.prettyPrint()
 
-    if (doPrettyPrint) {
-      info("Pretty-printing site.")
-      site.prettyPrint()
-    }
+        info("Verifying site.")
+        site.verify()
 
-    info("Verifying site.")
-    site.verify()
+      case "upload" =>
+        GoogleCloudStorageSynchronizer.sync(
+          serviceAccountKey = args(2),
+          Service.bucketName,
+          bucketPrefix = "",
+          directoryPath = siteRootPath + "/",
+          dryRun = (args.length > 3) && (args(3) == "dryRun")
+        )
 
-    if (doWrite) {
-      info("Writing site.")
-      site.writeStaticFiles()
+      case command => throw new IllegalArgumentException(s"Unrecognized command [$command]")
     }
   }
 }
